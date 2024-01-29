526c7148a8ddcdb8a7550330ed85788760583c90
Meezu Lawot <meezu.lawot@swifttech.com.np>
2024-01-29 13:58:58 +0545

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Meezu Lawot on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
